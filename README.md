cb35d6dafe7904ee48f69d2b07b65c6f1c659963
mqier21 <mqier21@gmail.com>
2024-06-28 23:59:15 +0800

    Amiami Purchase Bot

---
+++ README.md
@@ -0,0 +1,62 @@
+# amiamiPurchaseBot
+
+This is a simple [Amiami](https://www.amiami.com) automatically purchase script, it is based on selenium webdriver.
+
+## Prerequisites
+
+### Python Packages
+
+Run `pip install -r requirements.txt` to install necessary packages to execute this script.
+
+执行 `pip install -r requirements.txt` 安装脚本所需依赖
+
+若安装失败请指定你可用的镜像源后再重新尝试，如`pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt`
+
+### Chrome Driver
+
+This script was developed to work with the Google Chrome browser on the Microsoft Windows operating system. If you haven't installed chromedriver yet, read this [chromedriver](https://developer.chrome.com/docs/chromedriver/downloads/version-selection?hl=zh-cn).
+
+本脚本需要配合Chrome浏览器使用. 
+
+如果你未安装chromedriver，请按照以下网页说明根据Chrome版本进行下载 [chromedriver](https://developer.chrome.com/docs/chromedriver/downloads/version-selection?hl=zh-cn).
+
+## Configuration
+
+    email: the email address of your amiami account 
+    password: the password of your amiami account 
+	driverPath: the path of your Chrome driver application 
+	chromeUserData: the path of your Chrome user data, get on [chromeInfo](chrome://version/). Recommended to copy the user data directory (/Default) to a backup directory for use and be sure your account information is saved on chrome driver.
+	itemLink: you item link, please use eng url.
+
+please examine the source code yourself, make sure your account information will only be directly transmitted to Amiami's servers.
+
+
+	chromeUserData: 若不知道chrome用户数据目录路径请在这里个获取 [chromeInfo](chrome://version/).建议将用户数据目录(/Default)拷贝至一个备用目录进行使用，确保脚本通过chrome driver打开的浏览器已经存储你的AmiAmi个人账户信息（若缺少账户信息可以在脚本打开的浏览器中登录后重新运行脚本）.
+	itemLink: 该配置请使用英文链接,即请确保你的url中语言字段为eng.
+
+请检查源码确认你的账户信息只会传输至AmiAmi服务器
+
+## Execution
+
+Run the bot by issuing the following command in your terminal:
+
+```bash
+python ./main.py config.json
+```
+or
+```bash
+python ./main.py config.json debug
+```
+if use debug mode, automative procedures will halt at the penultimate "Confirm your order" screen.
+
+使用debug参数时，脚本会在最后一步停下来，在使用该脚本前请先尝试debug参数查看该脚本能否正常工作
+
+## Declaration
+
+By cloning, forking, contributing to, or running code in this repository, you are to assume all responsibilities for your actions. The authors of this repository are to claim no liability for any damage caused by malicious or inappropriate usage, in full or in part, of code in this repository.
+
+Please do not use this script for scalping, hoarding, or other similar activities
+
+通过克隆、分叉、贡献或运行此存储库中的代码，您将对您的操作承担所有责任。对于因恶意或不当使用此存储库中的全部或部分代码而造成的任何损害，此存储库的作者不承担任何责任。
+
+请勿将该脚本用于倒卖、囤积等行为
---
+++ amiamiBot.py
@@ -0,0 +1,134 @@
+import sys
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+
+def checkProduct(driver, productLink, credentials, debug):
+
+    driver.get(productLink)
+    cart = []
+
+    while len(cart) == 0:
+        
+        try:
+            driver.implicitly_wait(5)
+        except:
+            print("timed out")
+            pass
+        try:
+            elements = WebDriverWait(driver, 3).until(
+                lambda d: [elem for elem in d.find_elements(By.CLASS_NAME, "btn-cart")
+                          if elem.get_attribute("data-kanshi") == "productDetailAddCart9"]
+            )
+            print("element is:",elements)
+            cart = list(filter(lambda element: element is not None and 
+                element.get_attribute("style") == "", elements))
+
+            # check cart botton
+            if elements:
+                print("cart botton available")
+            else:
+                print("cart botton unavailable")
+
+        except:
+            print("out time, ",cart)
+            pass
+
+        # refreshing
+        if len(cart) == 0: 
+            time.sleep(2)
+            print("refreshing")
+            driver.refresh()
+        else:
+           pass
+    # add to cart        
+    cart[0].click()
+    print("added to cart successfully")
+
+    # proceed to checkout
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "cart_btn")))
+    elem_list = driver.find_elements(By.CLASS_NAME, "cart_btn")
+    elem_list[0].click()
+    print("proceed to checkout successfully")
+
+    # log in
+    email = driver.find_element(By.NAME, "email")
+    email.send_keys(credentials["email"])
+
+    passwd = driver.find_element(By.NAME, "password")
+    passwd.send_keys(credentials["password"])
+
+    submitButton = driver.find_element(By.CLASS_NAME, 'btn.btn-submit.btn-mini')
+    submitButton.click()
+
+    # continue checkout
+    print("click continue")
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-submit')))
+    submitButton2 = driver.find_element(By.CLASS_NAME, 'btn-submit')
+    print(submitButton2.get_attribute("data-kanshi"))
+    # driver.find_element(By.CLASS_NAME, 'btn-submit').click()
+
+    driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div/div/div/div[2]/section/div[4]/button[2]').click()
+    # driver.execute_script("document.getElementsByClassName('btn-submit')[0].click()")
+    print("click continue fin")
+    # Payment & shipping
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'shipping-method19')))
+
+    driver.execute_script("document.getElementById('shipping-method19').click()")
+    # unionpay
+    driver.execute_script("document.getElementById('payment-method11').click()")
+    driver.find_element(By.CLASS_NAME, "btn-submit").click()
+
+    # review
+    WebDriverWait(driver, 60).until(
+        EC.text_to_be_present_in_element((By.CLASS_NAME, 'section-title'), "Confirm your order"))
+    confirm = driver.find_element(By.CLASS_NAME, "btn-submit")
+    print("find the subimt botton:", confirm.text)
+    if not debug:
+        confirm.click() 
+
+        # if confirm.click() not available, try this 
+        # driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div/div/div/div[2]/section/div[3]/form/button').click()
+
+if __name__ == '__main__':
+
+
+    # argument
+    if len(sys.argv) < 2:
+        print("usage: amiamiBot.py config_path [debug]")
+        exit(1)
+
+    # configFile = "config.json"
+    debug = False
+    configFile = sys.argv[1]
+    if len(sys.argv) >= 3 and sys.argv[2] == "debug":
+        debug = True
+
+    with open(configFile) as cfg:
+        # load configuration files
+        config = json.load(cfg)
+        credentials = config["credentials"]
+        driverPath = config["driverPath"]
+        link = config["itemLink"]
+        userData = config["chromeUserData"]
+        print("config: ",config)
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_experimental_option("detach", True)
+        options.add_argument("user-data-dir="+userData)
+        options.add_argument("--ignore-certificate-errors")
+
+        service = Service(driverPath)
+        driver = webdriver.Chrome(options=options,
+                                  service=Service(driverPath))
+
+        checkProduct(driver, link, credentials, debug)
+
+        print("Completed")
